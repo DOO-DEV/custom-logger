098ff091d6d92f15278b3f3b689b1eb58cef90f0
DOO-DEV <nimo.vimo.yank@gmail.com>
2023-09-14 12:37:16 +0330

    feat: implement logger

--- logger/logger.go
+++ logger/logger.go
@@ -1,1 +1,28 @@
 package logger
+
+import (
+	"log"
+)
+
+type Logger struct {
+	logger *log.Logger
+}
+
+func New(l *log.Logger) *Logger {
+	return &Logger{logger: l}
+}
+
+func (l Logger) Warn(msg string) {
+	l.logger.SetPrefix("1: ")
+	l.logger.Println(msg)
+}
+
+func (l Logger) Info(msg string) {
+	l.logger.SetPrefix("2: ")
+	l.logger.Println(msg)
+}
+
+func (l Logger) Error(msg string) {
+	l.logger.SetPrefix("3: ")
+	l.logger.Println(msg)
+}
