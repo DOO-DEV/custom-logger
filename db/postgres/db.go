64a4f105260102f0deeeccc6ad0a041a24a45802
DOO-DEV <nimo.vimo.yank@gmail.com>
2023-09-14 12:36:52 +0330

    build: add db

--- db/postgres/db.go
+++ db/postgres/db.go
@@ -1,1 +1,23 @@
 package postgres
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+)
+
+type PgDB struct {
+	db *sql.DB
+}
+
+func New(dsn string) (*PgDB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	return &PgDB{db: db}, nil
+}
+
+func (d PgDB) CheckConnection() error {
+	return d.db.Ping()
+}
